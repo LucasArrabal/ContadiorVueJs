b8cf6afe33aa991dab387385b97ee5cfc89fabac
LucasArrabal <lucas.arrabal2003@gmail.com>
2023-03-14 11:02:58 -0300

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+export default{
+  data(){
+    return{
+      count : 0
+    }
+  },
+  methods:{
+    increment(){
+      this.count++
+    }
+  },
+  mounted(){
+    console.log(`The initial count is ${this.count}.`)
+  }
+}
+</script>
+
+<template>
+  <main class="app">
+    <h1>Indrotução a Vue.js</h1>
+    <h2>Contador:</h2>
+    <button @click="increment">Count is: {{ count }}</button>
+  </main>
+</template>
+
+
+
+
